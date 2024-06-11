61afeeadc1f9b91079ef754ffc4dff6440f73081
Samir <ss.dev.n98@gmail.com>
2024-06-11 17:33:19 +0545

    10: added take change section

--- theme/front-page.php
+++ theme/front-page.php
@@ -17,7 +17,30 @@ get_header();
 ?>
 
 <section class="container">
-  <h1>This is home page.</h1>
+  <div class="[&_p]:mb-0 [&_p]:opacity-70 [&_h2]:mb-5 lg:max-w-[560px]">
+    <p><span class="section-tag opacity-100">Take Change</span></p>
+    <h2>Making the most of your Vitality PLUS benefits</h2>
+    <p>With John Hancock Vitality, you’re in control. You can participate when and how you want</p>
+    <div class="my-10 space-x-6">
+      <a href="" class="btn btn-primary uppercase">Book Now</a>
+      <a href="tel:4422292409" class="btn btn-secondary">442-229-2409</a>
+    </div>
+    <div class="border-t border-primary pt-6 flex [&_p]:text-base [&_p]:leading-tight">
+      <!--<div class="border-t border-primary pt-6 flex [&_p]:text-base [&_p]:leading-tight relative after:absolute after:-left-5 after:border-l after:border-primary after:h-[70px]">-->
+      <div class="flex flex-col">
+        <i class="icon-hand-shake"></i>
+        <p>The more you engage, the more points you earn.</p>
+      </div>
+      <div class="flex flex-col">
+        <i class="icon-document"></i>
+        <p>Points determine your Vitality Status: Platinum, Gold, Silver or Bronze</p>
+      </div>
+      <div class="flex flex-col">
+        <i class="icon-trophy"></i>
+        <p>The higher your status, the greater the potential rewards!</p>
+      </div>
+    </div>
+  </div>
 </section>
 
 <?php
